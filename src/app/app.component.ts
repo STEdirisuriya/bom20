b48c530217c960bb18df331d9590b4bfda7731c3
STEdirisuriya <supunedirisuriya2002@gmail.com>
2019-11-12 15:52:35 +0530

    First update

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Live Score',
+      url: '/home',
+      icon: 'pulse'
+    },
+    {
+      title: 'Score Sheets',
+      url: '/list',
+      icon: 'grid'
+    },
+    {
+      title: 'Player Details',
+      url: '/details',
+      icon: 'people'
+    },
+    {
+      title: 'About',
+      url: '/about',
+      icon: 'information-circle'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
